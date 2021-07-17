1412e0b33d78a49b88a20b4b7cdb16601fc91133
KowalewskiPawel <53332984+KowalewskiPawel@users.noreply.github.com>
2021-07-17 23:10:46 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="MapScreen">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="MapScreen" component={MapScreen} />
       </Stack.Navigator>
